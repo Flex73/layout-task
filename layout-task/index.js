0fb5ae27c3550d58ed465e3f381bf544831353a7
Flex73 <flex7398@gmail.com>
2018-08-01 17:55:53 +0300

    Updated filter's implementation

--- layout-task/index.js
+++ layout-task/index.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const list = document.getElementById('filter');
-    const tag = document.getElementsByClassName('container__content__item__description__tags');
     const tags = document.getElementsByClassName('container__content__item__description__tags-item');
     const content = document.getElementById('content');
-    const contentId = document.getElementById('container');
 
     // Change color of buttons after click
     list.addEventListener('click', (ev) => {
@@ -16,13 +14,15 @@ document.addEventListener('DOMContentLoaded', () => {
         if (ev.target.tagName === 'LI') {
             // Filter HTMLCollection as Array
             const filteredCollection = Array.prototype.filter.call(tags, tags => tags.innerHTML === ev.target.innerHTML);
-                console.log(filteredCollection);
             // Check if elements have tag names and remove them if they don't
             if (filteredCollection.length === 0) {
                 while (content.firstChild) {
                     content.removeChild(content.firstChild);
                 }
             }
+            // Searching for parent's element to delete it
+            const a = filteredCollection[0].parentNode.parentNode.parentNode;
+            a.remove();
         }
     }
 
